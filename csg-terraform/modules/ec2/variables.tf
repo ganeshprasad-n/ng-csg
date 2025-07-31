e778c32d76828e34c824a0a926d920c8ab031275
Ganeshprasad <ganeshprasad.be@gmail.com>
2025-07-31 17:06:29 +0530

    commit

--- csg-terraform/modules/ec2/variables.tf
+++ csg-terraform/modules/ec2/variables.tf
@@ -21,7 +21,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
