8298d1d936c50f322c6525240585e1190d5900d5
Ganeshprasad <ganeshprasad.be@gmail.com>
2025-08-21 21:22:58 +0530

    1st

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "ng-terraform-bucket-2"
-    key            = "csg-uat/terraform.tfstate"      #Run "terraform init -reconfigure" if you changed backend config/location
-    region         = "us-east-2"
+    bucket         = "ganesh114"
+    key            = "ganeshprasad-csg/terraform/terraform.tfstate"
+    region         = "us-east-1"
 #   dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
