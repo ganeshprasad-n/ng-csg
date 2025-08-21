a2d7c329ae28fb777067dd3cc259af26f8ebcca2
ganeshprasad-n <141454647+ganeshprasad-n@users.noreply.github.com>
2025-08-21 17:14:09 +0530

    Update backend.tf
    disabled state lock

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "ng-terraform-bucket-2"
     key            = "csg-uat/terraform.tfstate"      #Run "terraform init -reconfigure" if you changed backend config/location
     region         = "us-east-2"
-    dynamodb_table = "terraform-lock-table"
+#   dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }
